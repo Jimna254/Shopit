21aa61e3c9be725abfae566f420c513d7d03d348
ShaBirgen <sharoncherotich112@gmail.com>
2024-02-29 15:56:43 +0300

    Footer added

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { RegisterComponent } from './Components/register/register.component';
 import { LoginComponent } from './Components/login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RegisterComponent, RouterLink],
+  imports: [CommonModule, RouterOutlet, RegisterComponent, RouterLink, LoginComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -2,17 +2,14 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './Components/register/register.component';
 import { LoginComponent } from './Components/login/login.component';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 export const routes: Routes = [
-  {
-    path: 'app',
-    component: AppComponent,
-    children: [
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent },
-    ],
-  },
+  { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'footer', component: FooterComponent },
 ];
